ae5c0e0a8cfd78723298d6a76d78d88091468f41
Dušan Lukić <dusanlukic404@gmail.com>
2022-05-23 15:02:02 +0200

    Update script.js

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,7 @@
 "use strict";
 
 //Library for drag and drop
-import Sortable from "sortablejs";
+// import Sortable from "sortablejs";
 
 // Images
 import imgCross from "../images/icon-cross.svg";
@@ -29,20 +29,20 @@ const headerImg = document.querySelector("#header-img");
 let theme = localStorage.getItem("theme");
 
 //For drag and drop
-const dragging = document.querySelector("#dragging-list");
-new Sortable(dragging, {
-  animation: 150,
-  ghostClass: "sortable-ghost",
-});
+// const dragging = document.querySelector("#dragging-list");
+// new Sortable(dragging, {
+//   animation: 150,
+//   ghostClass: "sortable-ghost",
+// });
 
-dragging.shouldCancelStart = function (e) {
-  if (
-    e.target.classList.contains("todo-check") ||
-    e.target.classList.contains("btn-delete-todo")
-  ) {
-    return true;
-  } else return false;
-};
+// dragging.shouldCancelStart = function (e) {
+//   if (
+//     e.target.classList.contains("todo-check") ||
+//     e.target.classList.contains("btn-delete-todo")
+//   ) {
+//     return true;
+//   } else return false;
+// };
 
 // Saving todos at local storage
 function saveToLocalStorage(todo) {
